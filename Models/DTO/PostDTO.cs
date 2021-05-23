0d3b3b55a3917221c2f40cff261eb49f3aae653c
vicenteloroxana <abigailvicentelo@gmail.com>
2021-05-22 22:56:59 -0300

    crear metodo y vistas  edit delete details

--- Models/DTO/PostDTO.cs
+++ Models/DTO/PostDTO.cs
@@ -12,7 +12,7 @@ namespace Blog.Models.DTO
         public int ID { get; set; }
         public string Titulo { get; set; }
         public string Contenido { get; set; }
-        public string Imagen { get; set; }
+        public byte[] Imagen { get; set; }
         public string Categoria { get; set; }
         public DateTime Fecha_de_creacion { get; set; }
     }
