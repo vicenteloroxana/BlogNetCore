3527ebf2860b0077d9469e5d51d3196771ef5747
vicenteloroxana <abigailvicentelo@gmail.com>
2021-05-25 22:08:30 -0300

    tests metodos create y details

---
+++ BasePruebas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Tests
+{
+    public class BasePruebas
+    {
+        protected BlogContext ConstruirContext(string nombreDB) {
+            var opciones = new DbContextOptionsBuilder<BlogContext>()
+                .UseInMemoryDatabase(nombreDB).Options;
+            var dbContext = new BlogContext(opciones);
+
+            return dbContext;
+        }
+    }
+}
---
+++ PruebasUnitarias/PostsControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Blog.Controllers;
+using Blog.Models;
+using Blog.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class PostsControllerTests: BasePruebas
+    {
+        [TestMethod]
+        public void TestViewCreate(){
+            PostsController controller = new PostsController();
+
+            ViewResult result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestMethodDetails(){
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+
+            contexto.Posts.Add(new Post(){
+                Titulo = "Informatica sigo XX",
+                Contenido = "Informatica",
+                Categoria = "Tecnologia",
+                Fecha_de_creacion = new DateTime(),
+                Imagen = new byte[1]
+            });
+            contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+
+            var controller = new PostsController(contexto2);
+            var respuesta = controller.Details(1) as ViewResult;
+            var post = (PostDTO) respuesta.ViewData.Model;
+
+            Assert.AreEqual("Informatica sigo XX", post.Titulo);
+        }
+    }
+}
